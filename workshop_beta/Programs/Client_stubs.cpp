65439f5ade4c0145270d0d6aeb26d57076eeede1
Shay Barak <shiber@gmail.com>
2012-08-14 20:38:07 +0300

    Auto formatting for client_stubs.cpp

--- workshop_beta/Programs/Client_stubs.cpp
+++ workshop_beta/Programs/Client_stubs.cpp
@@ -24,137 +24,137 @@ Player*					  player;
 
 void dbg_log(char* function_name, char* action)
 {
-  std::cout << "client, thread id " <<boost::this_thread::get_id()<<" " 
-            << "in function " << function_name
-            << " " <<action
-            <<std::endl;
+	std::cout << "client, thread id " <<boost::this_thread::get_id()<<" " 
+		<< "in function " << function_name
+		<< " " <<action
+		<<std::endl;
 }
 
 void move(double remaining_time)
 {
-  dbg_log("move", "entering");
-  
-  if (finished_game)
-    return;
+	dbg_log("move", "entering");
 
-  //here you should construct a motion
-  Motion motion_sequence;
-  player->move(remaining_time, motion_sequence);
-  
-  double motion_length(motion_sequence.motion_time( configuration.get_translational_speed(),
-                                                    configuration.get_rotational_speed()));
-  if (motion_length == 0) //no motion
-    return;
-  std::string path_filename;
+	if (finished_game)
+		return;
 
-  dbg_log("move", "requesting to write");
-  if (request_to_write(*socket_client_ptr, motion_length, path_filename))
-  {
-    dbg_log("move", "request granted");
-    //request granted
-    ofstream out(path_filename.c_str(), std::ios::app); //it is essential to append
-    motion_sequence.write(out);    
-  }
-  else
-  {
-    dbg_log("move", "request denied");
-  }
-  
-  dbg_log("move", "exiting");
-  return ;
+	//here you should construct a motion
+	Motion motion_sequence;
+	player->move(remaining_time, motion_sequence);
+
+	double motion_length(motion_sequence.motion_time( configuration.get_translational_speed(),
+		configuration.get_rotational_speed()));
+	if (motion_length == 0) //no motion
+		return;
+	std::string path_filename;
+
+	dbg_log("move", "requesting to write");
+	if (request_to_write(*socket_client_ptr, motion_length, path_filename))
+	{
+		dbg_log("move", "request granted");
+		//request granted
+		ofstream out(path_filename.c_str(), std::ios::app); //it is essential to append
+		motion_sequence.write(out);    
+	}
+	else
+	{
+		dbg_log("move", "request denied");
+	}
+
+	dbg_log("move", "exiting");
+	return ;
 }
 void moveable_planner(double remaining_time)
 {
-  dbg_log("moveable_planner", "entering");
-  
-  CGAL::Timer timer;
-  timer.start();
-  while (timer.time() < remaining_time)
-  {
-    //plan and then move (if time remains)
-    player->plan(remaining_time - timer.time());
-    if (remaining_time > timer.time())
-      move(remaining_time - timer.time());
-  }
+	dbg_log("moveable_planner", "entering");
 
-  dbg_log("moveable_planner", "exiting");
-  return;
+	CGAL::Timer timer;
+	timer.start();
+	while (timer.time() < remaining_time)
+	{
+		//plan and then move (if time remains)
+		player->plan(remaining_time - timer.time());
+		if (remaining_time > timer.time())
+			move(remaining_time - timer.time());
+	}
+
+	dbg_log("moveable_planner", "exiting");
+	return;
 }
 
 void static_planner(double remaining_time)
 {
-  dbg_log("static_planner", "entering");
+	dbg_log("static_planner", "entering");
 
-  CGAL::Timer timer;
-  timer.start();
-  while (timer.time() < remaining_time)
-  {
-    //plan
-    player->plan(remaining_time - timer.time());
-  }
+	CGAL::Timer timer;
+	timer.start();
+	while (timer.time() < remaining_time)
+	{
+		//plan
+		player->plan(remaining_time - timer.time());
+	}
 
-  dbg_log("static_planner", "exiting");
-  return;
+	dbg_log("static_planner", "exiting");
+	return;
 }
 
 void client_stubs_main(int argc, char* argv[])
 {
-  ////////////////////////////////////////////////////////////
-  
-  Env env(argc,argv);
-  Input_reader input_reader;
-  
+	////////////////////////////////////////////////////////////
 
-  ////////////////////////////////////////////////////////////
-  //loading scene from environment
-  //should be used in stub functions:
-  //  plan, move, construct_motion
-  Env::Polygon_vector&      workspace(env.get_workspace());
-  Env::Extended_polygon&    robot(env.get_robot_a());
-  Env::Extended_polygon&    dynamic_obstacle(env.get_robot_b());
+	Env env(argc,argv);
+	Input_reader input_reader;
 
-  Env::Reference_point      source_ref_p(env.get_source_configuration_a());
-  Env::Reference_point_vec* target_configurations_ptr = &(env.get_target_configurations());
-    
-  ////////////////////////////////////////////////////////////
-  //initialize connection to server
-  socket_client_ptr = new Socket_client(configuration.get_host_name(), configuration.get_host_port());
 
-  
-  ////////////////////////////////////////////////////////////
-  //begin planning
-  player = (Player*)new NaivePlayer(&env, &configuration);
-  bool read_additional_configurations = true;
-  while (finished_game == false)
-  {    
-    //before doing any planning, see if something changed
-    dbg_log("main client", "getting scene status");
-    Scene_status scene_status = get_scene_status(*socket_client_ptr); 
-	player->set_dynamic_obstacle_config(input_reader.read_reference_point<Rational_kernel>(scene_status.quasi_dynamic_obs_location_filename));
-    if (scene_status.updated_target_configurations && (read_additional_configurations) )
-    {
-      input_reader.read_reference_points<Rational_kernel>(scene_status.updated_target_configurations_filename,
-                                                          std::back_inserter(*target_configurations_ptr));
-      read_additional_configurations = false;
-	  player->update_target_configs(target_configurations_ptr);
-    }
-    
-    //now plan
-    dbg_log("main client", "planning");
-    Time_frame_status time_frame_status = get_time_frame_status(*socket_client_ptr); 
-    if (time_frame_status.is_moveable)
-      moveable_planner(time_frame_status.remaining_time);
-    else //(time_frame_status.is_moveable == false)
-      static_planner(time_frame_status.remaining_time);
+	////////////////////////////////////////////////////////////
+	//loading scene from environment
+	//should be used in stub functions:
+	//  plan, move, construct_motion
+	Env::Polygon_vector&      workspace(env.get_workspace());
+	Env::Extended_polygon&    robot(env.get_robot_a());
+	Env::Extended_polygon&    dynamic_obstacle(env.get_robot_b());
 
-    std::cout <<std::endl<<std::endl;
-  }
+	Env::Reference_point      source_ref_p(env.get_source_configuration_a());
+	Env::Reference_point_vec* target_configurations_ptr = &(env.get_target_configurations());
 
-  std::cout <<"FINISHED..."<<std::endl;
+	////////////////////////////////////////////////////////////
+	//initialize connection to server
+	socket_client_ptr = new Socket_client(configuration.get_host_name(), configuration.get_host_port());
 
-  terminate_connection(*socket_client_ptr);
-  delete (socket_client_ptr);
 
-  return;
+	////////////////////////////////////////////////////////////
+	//begin planning
+	player = (Player*)new NaivePlayer(&env, &configuration);
+	bool read_additional_configurations = true;
+	while (finished_game == false)
+	{    
+		//before doing any planning, see if something changed
+		dbg_log("main client", "getting scene status");
+		Scene_status scene_status = get_scene_status(*socket_client_ptr); 
+		player->set_dynamic_obstacle_config(input_reader.read_reference_point<Rational_kernel>(scene_status.quasi_dynamic_obs_location_filename));
+		if (scene_status.updated_target_configurations && (read_additional_configurations) )
+		{
+			input_reader.read_reference_points<Rational_kernel>(scene_status.updated_target_configurations_filename,
+				std::back_inserter(*target_configurations_ptr));
+			read_additional_configurations = false;
+			player->update_target_configs(target_configurations_ptr);
+		}
+
+		//now plan
+		dbg_log("main client", "planning");
+		Time_frame_status time_frame_status = get_time_frame_status(*socket_client_ptr); 
+		if (time_frame_status.is_moveable)
+			moveable_planner(time_frame_status.remaining_time);
+		else //(time_frame_status.is_moveable == false)
+			static_planner(time_frame_status.remaining_time);
+
+		std::cout <<std::endl<<std::endl;
+	}
+
+	std::cout <<"FINISHED..."<<std::endl;
+
+	terminate_connection(*socket_client_ptr);
+	delete (socket_client_ptr);
+
+	return;
 }
 
