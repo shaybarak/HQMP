fb704ad8d343e1143085cb7c60331b0a76278d05
Shay Barak <shiber@gmail.com>
2012-08-17 17:41:24 +0300

    Improved my_run.bat
    Now has titles for cmd windows for players

--- workshop_beta/Input/my_run.bat
+++ workshop_beta/Input/my_run.bat
@@ -1,5 +1,8 @@
-start server configuration.txt
+echo off
+cls
+start "Server" server configuration.txt
 start mrmp
+echo Please instruct GUI to connect to server...
 pause
-start client configuration_a.txt
-start client configuration_b.txt
+start "Player A" client configuration_a.txt
+start "Player B" client configuration_b.txt
