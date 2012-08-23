7d41730885388cd81c131253f76fd99568c68fc4
Noam <noaminbar510@gmail.com>
2012-08-23 15:08:44 +0300

    Changed client so that no CGAL preconditions will take place

--- workshop_beta/client/main.cpp
+++ workshop_beta/client/main.cpp
@@ -1,6 +1,11 @@
 #include "stdafx.h"
 #include "..\Programs\Client_stubs.h"
 
+
+//TODO: remove!!
+#define CGAL_NO_PRECONDITIONS
+#define CGAL_NO_POSTCONDITIONS
+
 int main(int argc, char* argv[]) {
 	client_stubs_main(argc, argv);
 	return 0;
