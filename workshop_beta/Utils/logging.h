04516d59f38ee5f8563171c0fa599ba08546264f
Noam <noaminbar510@gmail.com>
2012-09-01 12:30:16 +0300

    Added some logging

--- workshop_beta/Utils/logging.h
+++ workshop_beta/Utils/logging.h
@@ -26,6 +26,7 @@
 	#define PRINT_FA_FSC_PREFIX() cout << "FA fsc " << fa_fsc_index << " of " << fa_fsc_count << " "; print_fixed_angle_fsc(*fsc_iter1, false)
 	#define PRINT_FA_FSC_SUFFIX() if (ccp_count != 0) {cout << " created " << ccp_count << " points";}cout << endl
 	#define	PRINF_FA_FSC_FAILED_GENERATING_POINT()	cout << endl << "Failed generating point, skipping connection"
+	#define PRINT_POLYGON_UNBOUNDED_SKIPPING() cout << "polygon is unbounded, skipping connection" << endl
 
 #else
 	#define PRINT_CONNECTORS() print_connectors_count()
@@ -33,16 +34,17 @@
 	#define PRINT_FA_FSC_PREFIX()
 	#define PRINT_FA_FSC_SUFFIX()
 	#define PRINF_FA_FSC_FAILED_GENERATING_POINT()
+	#define PRINT_POLYGON_UNBOUNDED_SKIPPING()
 
 #endif
 
 #ifdef PLAYBACK_MODE
-#define PLAYBACK_PRINT_CONNECTORS() print_connectors_as_configurations()
-#define PLAYBACK_PRINT_FIXED_ANGLE_FSCS() print_fixed_angle_fscs(true)
+	#define PLAYBACK_PRINT_CONNECTORS() print_connectors_as_configurations()
+	#define PLAYBACK_PRINT_FIXED_ANGLE_FSCS() print_fixed_angle_fscs(true)
 
 #else
-#define PLAYBACK_PRINT_CONNECTORS()
-#define PLAYBACK_PRINT_FIXED_ANGLE_FSCS()
+	#define PLAYBACK_PRINT_CONNECTORS()
+	#define PLAYBACK_PRINT_FIXED_ANGLE_FSCS()
 #endif
 
 void timed_trace(const char* function_name, const char* full_path, int line, const char* action);
