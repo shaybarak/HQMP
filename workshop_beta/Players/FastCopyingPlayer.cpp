4afc727430ba0a5fb395b9d75ff7bf43bf824040
Shay Barak <shiber@gmail.com>
2012-09-04 14:00:05 +0300

    Fixed a bug introduced in the previous commit

--- workshop_beta/Players/FastCopyingPlayer.cpp
+++ workshop_beta/Players/FastCopyingPlayer.cpp
@@ -98,7 +98,9 @@ bool FastCopyingPlayer::move(double deadline, Motion& motion_output) {
 		// Cut the motion
 		new_motion.cut_motion(deadline - timer.time(), motion_output);
 		// Update location to end of output motion
-		location = motion_output.back()->target();
+		if (!motion_output.empty()) {
+			location = motion_output.back()->target();
+		}
 		is_last_motion_complete = false;
 		timed_message("Moving to target but couldn't reach within time limit.");
 	}
